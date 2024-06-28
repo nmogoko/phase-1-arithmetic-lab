c47b2d80bfb8b848eb2effe77c2135ca1b0cb097
nmogoko <+>
2024-06-28 17:34:57 +0100

    Completed lab

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 
 // Write your code here
+
+let num1 = 31 ;
+let num2 = 2 ;
+let multiply = num1 * num2 ; 
+
+console.log(multiply);
+
+let random = Math.round(Math.random() *10 ) + 1;
+ console.log(random);
+
+ let num3 = 5
+ let num4 = 9
+ let mod = num4 % num3 ;
+ console.log(mod)
+
+ let max = Math.max(15,16,18,20);
+ console.log(max);
